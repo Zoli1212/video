7818195341a1b3bb451a3e568e3087f410f0a0d7
Zoli1212 <43321960+Zoli1212@users.noreply.github.com>
2021-02-13 20:06:50 +0100

    Update views.py

--- halls/views.py
+++ halls/views.py
@@ -18,7 +18,7 @@ YOUTUBE_API_KEY = 'AIzaSyDE3FR4egeLJUQ09CwssSCtvzdGQNe9tLs'
 
 def home(request):
     recent_halls = Hall.objects.all().order_by('-id')[:3]
-    popular_halls = [Hall.objects.get(pk=1),Hall.objects.get(pk=2),Hall.objects.get(pk=3)]
+    popular_halls = [Hall.objects.get(pk=7),Hall.objects.get(pk=8),Hall.objects.get(pk=9)]
     return render(request, 'halls/home.html', {'recent_halls':recent_halls, 'popular_halls':popular_halls})
 
 @login_required
